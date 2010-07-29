ba842936e752afd5a60f9a1d36c0da1a26f909f3
Denis Larionov <fairness@mail.ru>
2010-07-29 15:28:07 +0400

    Update settings

--- .setup/mysettings.py
+++ .setup/mysettings.py
@@ -2,5 +2,12 @@ DATABASE_ENGINE = 'postgresql_psycopg2'
 DATABASE_NAME = 'myprogs' 
 DATABASE_USER = 'postgres'
 DATABASE_PASSWORD = 'PgAdmin2010'
-TEMPLATE_DIRS = 'd:/6.PROJECTS/sport/src/djtrunk/sport/templates'
-STATIC_DOC_ROOT = 'd:/6.PROJECTS/sport/src/djtrunk/sport/templates/styles'
+TEMPLATE_DIRS = 'd:/6.PROJECTS/myprogs/src/trunk/templates'
+STATIC_DOC_ROOT = 'd:/6.PROJECTS/myprogs/src/trunk/templates/styles'
+
+# Или такой вариант:
+
+DATABASE_ENGINE = 'django.db.backends.sqlite3'
+DATABASE_NAME = 'd:/6.PROJECTS/myprogs/src/db/sqlite3.db' 
+TEMPLATE_DIRS = 'd:/6.PROJECTS/myprogs/src/trunk/templates'
+STATIC_DOC_ROOT = 'd:/6.PROJECTS/myprogs/src/trunk/templates/styles'
